c5495b8174f8cbcd2d861f84bf6364ce0ab855d6
_oah <782773467@qq.com>
2023-02-25 09:44:22 +0800

    first commit

---
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -0,0 +1,106 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author _oah
+ * @Date 2023.2.25 8:38
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public String hello() {
+        System.out.println("hello world...");
+        return "OK";
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/com/itheima/testdeploy/RedeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.testdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
